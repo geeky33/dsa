8f77a3079531b0bba6c05831ab5737986a321f57
nahh <141430616+geeky33@users.noreply.github.com>
2024-10-16 01:36:18 +0530

    Create patterns2.cpp

---
+++ patterns2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void nForest() {
+    int N;
+	cout<<"Enter N"<<endl;
+	cin>>N;
+    int i,j;
+	for(i=0;i<N;i++){
+		for(j=0;j<=i;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+
+	}
+}
+int main(){
+	nForest();
+}
